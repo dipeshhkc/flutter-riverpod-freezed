7aeade2ec936216769e3481b94074e24c45fa8d1
Dipesh K.C <kcdipesh429@gmail.com>
2021-08-18 18:14:13 +0545

    feat:add Custom Exception

---
+++ lib/exception/custom_exception.dart
@@ -0,0 +1,7 @@
+class CustomException implements Exception {
+  final String? message;
+  const CustomException({this.message = "Something went wrong!"});
+
+  @override
+  String toString() => 'CustomException(message: $message)';
+}
